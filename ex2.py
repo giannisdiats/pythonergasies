928a655499b9c0219e70b49c67fb70fc344281bc
giannisdiats <giannisdiats@outlook.com>
2017-10-15 21:07:34 +0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,146 @@
+time=0
+max1=0
+maxt=0
+
+print("dwse kinhsh gia ths 4 gia kathe 10 lepta")
+while(time<6):
+        
+        kin=input(int)
+        
+       
+        time=time+1
+        if int(kin)>max1 :
+            maxt=time
+            max1=int(kin)
+           
+       
+
+if maxt==1:
+    g=[max1,"4:00"]
+    print (g)
+if maxt==2:
+    g=[max1,"4:10"]
+    print (g)
+if maxt==3:
+    g=[max1,"4:20"]
+    print (g)
+if maxt==4:
+    g=[max1,"4:30"]
+    print (g)
+if maxt==5:
+    g=[max1,"4:40"]
+    print (g)
+if maxt==6:
+    g=[max1,"4:50"]
+    print (g)
+time=0
+max1=0
+maxt=0
+print("dwse kinhsh gia ths 5 gia kathe 10 lepta")
+while(time<6):
+        kin=input(int)
+        
+       
+        time=time+1
+        if int(kin)>max1 :
+            maxt=time
+            max1=int(kin)
+           
+       
+
+if maxt==1:
+    t=[max1,"5:00"]
+    print (t)
+if maxt==2:
+    t=[max1,"5:10"]
+    print (t)
+if maxt==3:
+    t=[max1,"5:20"]
+    print (t)
+if maxt==4:
+    t=[max1,"5:30"]
+    print (t)
+if maxt==5:
+    t=[max1,"5:40"]
+    print (t)
+if maxt==6:
+    t=[max1,"5:50"]
+    print (t)
+
+time=0
+max1=0
+maxt=0
+print("dwse kinhsh gia ths 6 gia kathe 10 lepta")
+while(time<6):
+        kin=input(int)
+        
+       
+        time=time+1
+        if int(kin)>max1 :
+            maxt=time
+            max1=int(kin)
+           
+       
+
+if maxt==1:
+    rin=[max1,"6:00"]
+    print (rin)
+if maxt==2:
+    rin=[max1,"6:10"]
+    print (rin)
+if maxt==3:
+    rin=[max1,"6:20"]
+    print (rin)
+if maxt==4:
+    rin=[max1,"6:30"]
+    print (rin)
+if maxt==5:
+    rin=[max1,"6:40"]
+    print (rin)
+if maxt==6:
+    rin=[max1,"6:50"]
+    print (rin)
+time=0
+max1=0
+maxt=0
+print("dwse kinhsh gia ths 7 gia kathe 10 lepta")
+while(time<6):
+        kin=input(int)
+        
+       
+        time=time+1
+        if int(kin)>max1 :
+            maxt=time
+            max1=int(kin)
+            
+       
+
+if maxt==1:
+    n=[max1,"7:00"]
+    print (n)
+if maxt==2:
+    n=[max1,"7:10"]
+    print (n)
+if maxt==3:
+    n=[max1,"7:20"]
+    print (n)
+if maxt==4:
+    n=[max1,"7:30"]
+    print (n)
+if maxt==5:
+    n=[max1,"7:40"]
+    print (n)
+if maxt==6:
+    n=[max1,"7:50"]
+    print (n)
+k=[g,t,rin,n]
+print("to apotelesma einai:")
+print (k)
+input("Press Enter to continue...")
+
+
+
+
+
+
+
